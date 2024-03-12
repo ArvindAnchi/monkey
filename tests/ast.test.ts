f6b214503daf87877b0147017dbd96e4d5b8d2d6
Arvind Anchi <arvindanchi@gmail.com>
2024-03-12 22:14:19 +0400

    test: Change to vitest

--- tests/ast.test.ts
+++ tests/ast.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from '@jest/globals'
+import { describe, test, expect } from 'vitest'
 import { Identifier, LetStatement, Program } from '../ast'
 import { Token } from '../token'
 
