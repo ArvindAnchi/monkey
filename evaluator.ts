c68c468c274c99f6dd93c4e0a3e6d01542b62ad3
Arvind Anchi <arvindanchi@gmail.com>
2024-03-23 12:41:29 +0400

    feat: Add evaluator for 'Program' and 'ExpressionStatement'

--- evaluator.ts
+++ evaluator.ts
@@ -1,7 +1,25 @@
 import * as obj from './objects'
 import * as ast from './ast'
 
-export function Eval(node: ast.Node) {
+function evalStatemets(stmts: ast.Statement[]): obj.MObject | null {
+    let result: obj.MObject | null = null
+
+    for (const stmt of stmts) {
+        result = Eval(stmt)
+    }
+
+    return result
+}
+
+export function Eval(node: ast.Node | null) {
+    if (node instanceof ast.Program) {
+        return evalStatemets(node.statements)
+    }
+
+    if (node instanceof ast.ExpressionStatement) {
+        return Eval(node.expression)
+    }
+
     if (node instanceof ast.IntegerLiteral) {
         return new obj.Integer(node.value)
     }
