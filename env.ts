aa2667388666a28a7519fcbcaf5c0766fd85d252
Arvind Anchi <arvindanchi@gmail.com>
2024-03-28 19:52:31 +0400

    feat: Add nested environments

--- env.ts
+++ env.ts
@@ -2,13 +2,19 @@ import { Err, MObject } from "./objects";
 
 export class Environment {
     private store: Record<string, MObject>
+    private outer: Environment | null
 
-    constructor() {
+    constructor(outer?: Environment) {
         this.store = {}
+        this.outer = outer ?? null
     }
 
-    get(name: string) {
+    get(name: string): MObject {
         if (!(name in this.store)) {
+            if (this.outer != null && name in this.outer) {
+                return this.outer.get(name)
+            }
+
             return new Err(`identifier not found: ${name}`)
         }
 
